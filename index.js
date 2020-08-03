41c0a84ac3b2649c35bc0064051042f08d6291fd
Siddharth Kumar Jain <s8002.sid@gmail.com>
2020-08-03 09:32:59 +0530

    Adding logging functionality

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const app = express();
+const bunyan = require('bunyan');
+const {LoggingBunyan} = require('@google-cloud/logging-bunyan');
+const loggingBunyan = new LoggingBunyan();
+const logger = bunyan.createLogger({
+  // The JSON payload of the log as it appears in Stackdriver Logging
+  // will contain "name": "my-service"
+  name: 'my-service',
+  streams: [
+    // Log to the console at 'info' and above
+    {stream: process.stdout, level: 'info'},
+    // And log to Stackdriver Logging, logging at 'info' and above
+    loggingBunyan.stream('info'),
+  ],
+});
+
 
 app.get('/', (req, res) => {
   console.log('Hello world received a request.');
 
   const target = process.env.TARGET || 'Sid';
   res.send(`Hello ${target}!`);
+  logger.info('route / called');
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log('Hello world listening on port', port);
+  logger.info('Hello world listening on port', port);
 });
+logger.info('Server is ready...');
