44a2ded4ccfd9cbb2b0a23f4d676c6ef712bf140
Siddharth Kumar Jain <s8002.sid@gmail.com>
2020-08-08 18:31:04 +0530

    Testing google oAuth 2.0 using passport

--- index.js
+++ index.js
@@ -38,7 +38,45 @@ app.post('/job/submit', multer.single('file'), (req, res, next) => {
   .catch((e) => {res.status(404).send({status: "error"});})
 });
 
+/******************************************/
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+    clientID: '462043570647-jhv9unafh5kst6odo7ktd56j642n31kf.apps.googleusercontent.com',
+    clientSecret: 'sr67FJd41of4pgxcr6bEsceh',
+    callbackURL: "https://gc-helloworld-v4vl4rp6dq-uc.a.run.app/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+  }
+));
+
+// GET /auth/google
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Google authentication will involve
+//   redirecting the user to google.com.  After authorization, Google
+//   will redirect the user back to this application at /auth/google/callback
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
+
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+/******************************************/
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   logger.info('Hello world listening on port', port);
